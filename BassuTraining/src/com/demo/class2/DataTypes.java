831e9ac70bc5f064e9f1ee17a67ad20a1db7a993
Anurag Suri <sskanurag69@gmail.com>
2016-09-19 11:54:20 -0500

    First change for demo class 2 data types

--- BassuTraining/src/com/demo/class2/DataTypes.java
+++ BassuTraining/src/com/demo/class2/DataTypes.java
@@ -1,12 +1,15 @@
 package com.demo.class2;
 
 public class DataTypes {
-	int i=10;
+int i=10;
 	long l =10;
 	float f = 10.0f;
 	double d= 10.0;
 	String dd= "sss";
 	Long ll;
 	Double ddd;
-	
+public static void main (String[]args){
+	DataTypes dt = new DataTypes();
+	System.out.println("This is the first change for git"+dt.i);
+}
 }
